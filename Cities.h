254dfe4f6ec782cea5a2764340396531e4c22b6d
jpaulite91 <joeven.paulite@gmail.com>
2013-10-29 11:37:50 -0700

    Yellow Cities Done

--- Cities.h
+++ Cities.h
@@ -83,6 +83,70 @@ void cities()
 	earth[11].connectThree = &earth[9]; // Essen
 
 	//Yellow Cities 12-23
+	earth[12].name = "Los Angeles";
+	earth[12].connectOne = &earth[0]; // SF
+	earth[12].connectTwo = &earth[1]; // Chicago
+	earth[12].connectTwo = &earth[47]; // Sydney
+	earth[12].connectThree = &earth[13]; // Mexico City
+	
+	earth[13].name = "Mexico City";
+	earth[13].connectOne = &earth[12]; // LA
+	earth[13].connectTwo = &earth[1]; // Chicago
+	earth[13].connectTwo = &earth[14]; // Miami
+	earth[13].connectTwo = &earth[15]; // Bogota
+	earth[13].connectThree = &earth[16]; // Lima
+	
+	earth[14].name = "Miami";
+	earth[14].connectOne = &earth[0]; // SF
+	earth[14].connectTwo = &earth[1]; // Chicago
+	earth[14].connectTwo = &earth[15]; // Bogota
+	earth[14].connectThree = &earth[4]; // Washington
+	
+	earth[15].name = "Bogota";
+	earth[15].connectOne = &earth[14]; // Miami
+	earth[15].connectTwo = &earth[16]; // Lima
+	earth[15].connectTwo = &earth[13]; // Mexico City 
+	earth[15].connectThree = &earth[18]; // Sau Paulo
+	earth[15].connectTwo = &earth[17]; // Buenos Aires
+	
+	earth[16].name = "Lima";
+	earth[16].connectOne = &earth[15]; // Bogota
+	earth[16].connectTwo = &earth[13]; // Mexico City 
+	earth[16].connectThree = &earth[23]; // Santiago
+	
+	earth[17].name = "Buenos Aires";
+	earth[17].connectThree = &earth[18]; // Sau Paulo
+	earth[17].connectTwo = &earth[15]; // Bogota
+	
+	earth[18].name = "Sao Paulo";
+	earth[18].connectOne = &earth[7]; // Madrid
+	earth[18].connectTwo = &earth[19]; // Lagos
+	earth[18].connectThree = &earth[15]; // Bogota
+	earth[18].connectTwo = &earth[17]; // Buenos Aires
+	
+	earth[19].name = "Lagos";
+	earth[19].connectOne = &earth[18]; // Sao Paulo
+	earth[19].connectTwo = &earth[20]; // Khartoum
+	earth[19].connectThree = &earth[22]; // Kinshasa
+	
+	earth[20].name = "Khartoum";
+	earth[20].connectOne = &earth[19]; // Lagos
+	earth[20].connectTwo = &earth[21]; // Johannesburg
+	earth[20].connectThree = &earth[22]; // Kinshasa
+	earth[20].connectOne = &earth[26]; // Cairo
+	
+	earth[21].name = "Johannesburg";
+	earth[21].connectTwo = &earth[20]; // Khartoum
+	earth[21].connectThree = &earth[22]; // Kinshasa
+	
+	earth[22].name = "Kinshasa";
+	earth[22].connectOne = &earth[19]; // Lagos
+	earth[22].connectTwo = &earth[21]; // Johannesburg
+	earth[22].connectThree = &earth[20]; // Khartoum
+	
+	earth[23].name = "Santiago";
+	earth[23].connectTwo = &earth[16]; // Lima
+
 	//Black Cities 24-35
 	//Red Cities 36-47
 }
